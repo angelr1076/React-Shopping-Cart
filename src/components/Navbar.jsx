15a28550d7bfdabcbf42d2b20644a3c996bfb676
angelr1076 <angelr1076@gmail.com>
2024-01-06 14:34:02 -0500

    Added new logos and styled navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../contexts/CartContext';
+import SantaCarlaLogo from '../assets/images/logo-transparent-png.png';
 import '../styles/Navbar.css';
 
 function Navbar() {
@@ -12,15 +13,23 @@ function Navbar() {
 
   return (
     <nav className='navbar'>
-      <div className='logo-placeholder'>Logo</div>
-      <div className='navbar-links'>
-        <Link to='/'>Home</Link> |<Link to='/shop'>Shop</Link> |
-        <Link to='/cart'>
-          Cart
-          <span className='navbar__cart'>{totalItems}</span>
+      <div className='logo-placeholder'>
+        <Link to='/'>
+          <img
+            src={SantaCarlaLogo}
+            style={{ width: '50px', height: 'auto' }}
+            alt='Santa Carla Logo'
+          />
         </Link>
       </div>
-      <div className='navbar-spacer'></div>
+      <div className='navbar-shop'>
+        <Link to='/shop'>Shop</Link>
+      </div>
+      <div className='navbar-cart'>
+        <Link to='/cart'>
+          Cart | <span className='navbar__cart'>{totalItems}</span>
+        </Link>
+      </div>
     </nav>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,23 @@
 import '../styles/HomePage.css';
+import logo from '../assets/images/logo-transparent-png.png';
 
 function HomePage() {
   return (
     <div className='homepage'>
-      <h1>Welcome to Santa Carla. Murder capital of the world.</h1>
+      <h1>Welcome to Santa Carla. Sleep all day, surf all night.</h1>
+      <section className='hero border'>
+        <img
+          src={logo}
+          style={{ width: '400px', height: 'auto' }}
+          alt='Logo'
+          className='logo'
+        />
+        <p className='description'>
+          Dedicated to the (night)lifestyle of Santa Carla, its boardwalk, and
+          its residents. Here you can find information about the best surf
+          spots, local events, and more.
+        </p>
+      </section>
     </div>
   );
 }
