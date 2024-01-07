fc29e7df556194a39ebd38ce7dc4147d8cdbf13d
angelr1076 <angelr1076@gmail.com>
2024-01-07 16:27:20 -0500

    Added montserrat font and cleaned up homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../styles/HomePage.css';
 import logo from '../assets/images/logo-transparent-png.png';
 
@@ -8,17 +9,24 @@ function HomePage() {
         Welcome to Santa Carla. Sleep all day, surf all night.
       </h1>
       <section className='hero border'>
-        <img
-          src={logo}
-          style={{ width: '400px', height: 'auto' }}
-          alt='Logo'
-          className='logo'
-        />
-        <p className='description'>
-          Dedicated to the (night)lifestyle of Santa Carla, its boardwalk, and
-          its residents. Soon, you'll find information about the best surf
-          spots, local events, and more.
-        </p>
+        <div className='hero-content'>
+          <img
+            src={logo}
+            style={{ width: '400px', height: 'auto' }}
+            alt='Logo'
+            className='logo'
+          />
+          <div className='description-content'>
+            <p className='description'>
+              Dedicated to the (night)lifestyle of Santa Carla, its boardwalk,
+              and its residents. Soon, you'll find information about the best
+              surf spots, local events, and more.
+            </p>
+            <Link to='/shop' className='shop-button'>
+              Shop
+            </Link>
+          </div>
+        </div>
       </section>
     </div>
   );
