7e0b5ad84ec60b9b85f8cef41719ec96ad7ca44a
angelr1076 <angelr1076@gmail.com>
2024-01-10 09:27:00 -0500

    Set cart container height to 100vh min. Centered and added a card type background. Added button container to cart buttons.

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,22 +1,34 @@
 import { useContext } from 'react';
 import { CartContext } from '../contexts/CartContext';
+import { FiMinusCircle, FiPlusCircle } from 'react-icons/fi';
+import '../styles/CartPage.css';
 
 function CartPage() {
   const { cartItems, removeFromCart, updateQuantity, clearCart } =
     useContext(CartContext);
 
   const renderCartItem = item => (
-    <div key={item.product.id}>
-      <h3>{item.product.title}</h3>
-      <p>Quantity: {item.quantity}</p>
-      <p>Price: ${item.product.price}</p>
-      <button onClick={() => updateQuantity(item.product, item.quantity - 1)}>
-        -
-      </button>
-      <button onClick={() => updateQuantity(item.product, item.quantity + 1)}>
-        +
-      </button>
-      <button onClick={() => removeFromCart(item.product)}>Remove</button>
+    <div key={item.product.id} className='cart-item'>
+      <h4>{item.product.title}</h4>
+      <p>{item.quantity}</p>
+      <p>${item.product.price}</p>
+      <div className='cart-buttons'>
+        <button
+          className='icon minus btn'
+          onClick={() => updateQuantity(item.product, item.quantity - 1)}>
+          <FiMinusCircle />
+        </button>
+        <button
+          className='icon plus btn'
+          onClick={() => updateQuantity(item.product, item.quantity + 1)}>
+          <FiPlusCircle />
+        </button>
+        <button
+          className='buy btn'
+          onClick={() => removeFromCart(item.product)}>
+          Remove From Cart
+        </button>
+      </div>
     </div>
   );
 
@@ -30,8 +42,13 @@ function CartPage() {
       <h2>My Cart</h2>
       {cartItems.length > 0 ? (
         <>
-          {cartItems.map(renderCartItem)}
-          <button onClick={handleCheckout}>Checkout</button>
+          <div className='items'>{cartItems.map(renderCartItem)}</div>
+          <hr className='cart-item-divider' />
+          <div className='cart-buttons'>
+            <button className='checkout btn' onClick={handleCheckout}>
+              Checkout
+            </button>
+          </div>
         </>
       ) : (
         <p>Your cart is empty.</p>
