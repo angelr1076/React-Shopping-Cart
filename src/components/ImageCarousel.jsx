893549ba0bd657550ad1e99ae7a78c3e1c45e987
angelr1076 <angelr1076@gmail.com>
2024-01-27 16:19:48 -0500

    Added images to the ImageCarousel from the ProductService API and removed the carousels visible scrollbar

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,56 +1,36 @@
+import { useState, useEffect } from 'react';
 import '../styles/ImageCarousel.css';
+import fetchBoards from '../services/ProductService';
 
 function ImageCarousel() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const loadBoards = async () => {
+      try {
+        const boards = await fetchBoards();
+        const boardImages = boards.data.map(board => {
+          return {
+            name: board[6].value, // Name of image
+            src: board[14].value, // URL of image
+          };
+        });
+        setImages(boardImages);
+      } catch (error) {
+        console.error('Failed to fetch boards:', error);
+      }
+    };
+
+    loadBoards();
+  }, []);
+
   return (
     <div className='carousel'>
-      <div className='carousel-image'>
-        <img
-          src='https://res.cloudinary.com/angelrodriguez/image/upload/v1706296495/Santa%20Carla%20Surf/billboard-closeup.jpg'
-          alt='Billboard of Santa Carla'
-        />
-      </div>
-      <div className='carousel-image'>
-        <img
-          src='https://res.cloudinary.com/angelrodriguez/image/upload/v1706296685/Santa%20Carla%20Surf/santa-carla-coaster2.jpg'
-          alt='Coaster Image 2'
-        />
-      </div>
-      <div className='carousel-image'>
-        <img
-          src='https://res.cloudinary.com/angelrodriguez/image/upload/v1706296273/Santa%20Carla%20Surf/boardwalk-sign.jpg'
-          alt='Boardwalk Sign'
-        />
-      </div>
-      <div className='carousel-image'>
-        <img
-          src='https://res.cloudinary.com/angelrodriguez/image/upload/v1706296771/Santa%20Carla%20Surf/tour-image.jpg'
-          alt='Tour Image'
-        />
-      </div>
-      <div className='carousel-image'>
-        <img
-          src='https://res.cloudinary.com/angelrodriguez/image/upload/v1706296336/Santa%20Carla%20Surf/billboard-illustrated.jpg'
-          alt='Illustrated Billboard'
-        />
-      </div>
-      <div className='carousel-image'>
-        <img
-          src='https://res.cloudinary.com/angelrodriguez/image/upload/v1706296235/Santa%20Carla%20Surf/lighthouse-coast.jpg'
-          alt='Lighthouse Coast'
-        />
-      </div>
-      <div className='carousel-image'>
-        <img
-          src='https://res.cloudinary.com/angelrodriguez/image/upload/v1706296201/Santa%20Carla%20Surf/boardwalk-daytime.jpg'
-          alt='Boardwalk Daytime'
-        />
-      </div>
-      <div className='carousel-image'>
-        <img
-          src='https://res.cloudinary.com/angelrodriguez/image/upload/v1706296068/Santa%20Carla%20Surf/santa-carla-coaster.jpg'
-          alt='Coaster Image 1'
-        />
-      </div>
+      {images.map((image, index) => (
+        <div className='carousel-image' key={index}>
+          <img src={image.src} alt={image.name} />
+        </div>
+      ))}
     </div>
   );
 }
